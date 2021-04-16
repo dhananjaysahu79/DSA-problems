6655234386fd415de203827a6376a665c5ac47c4
Dhananjay <49968050+dhananjaysahu79@users.noreply.github.com>
2021-04-16 19:29:08 +0530

    kadane's algorithm

--- Kadane's Algorithm.py
+++ Kadane's Algorithm.py
@@ -7,7 +7,7 @@ s = 0 #variable to calculate at each index
 curr_max = arr[0] #variable to store maximum at each index
 for i in arr:
     s += i
-    if s > curr_max: curr_max = s  # at any index if sum is greater than the current maximum we will swap it.
+    if s > curr_max: curr_max = s  # at any index if sum is greater than the current maximum we will update it to curr_max.
     if s < 1: s = 0 # if at any index sum is lesser than 1 we have to leave that subsequence,if not it will reduce the sum in further subsequence.
 print(curr_max)
 
