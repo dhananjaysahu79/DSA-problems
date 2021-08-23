7946aafa76d50468906365bdc7804a76d2dbb043
Dhananjay <49968050+dhananjaysahu79@users.noreply.github.com>
2021-08-23 22:47:05 +0530

    sc

--- Linkedlist/linkedlist basics functionalities.py
+++ Linkedlist/linkedlist basics functionalities.py
@@ -7,12 +7,13 @@ class LinkedList:
         self.head = None
         self.tail = None
 
-        
+
     #inserting the data at the start/begining.
     #three things we will have to do:
     # 1: creating the Node
-    # 2: store the adress of current head node, since we are inserting at start so to will point to the current              head Node
-    # 3: change the current head node to the newly added node as its now at the start
+    # 2: store the adress of current head node, since we are inserting at start
+    # so to will point to the current head Node
+    # 3: change the current head node to the newly added nodeas its now at the start
     def insertAtStart(self, data):
         node = Node(data,self.head)
         self.head = node
@@ -27,10 +28,39 @@ class LinkedList:
         self.tail = node
 
 
-    #inserting data at the middle.
-    #deleting the data at the start/begining.
-    #deleting data at the end.
-    #deleting data at the middle.
+    #inserting data at the anywhere.
+
+    # 1.traverse to that index
+    # 2.create a node with data and next pointing to the index node.
+    # 3.set next of its previous node of the index to the newly created node.
+
+
+    def insertAtindex(self, data, index):
+        itr = self.head
+        prev = self.head
+        c = 1
+        while itr:
+            if c == index:
+                node = Node(data, itr)
+                if index == 1:
+                    self.head = node
+                if index != 1:
+                    prev.next = node
+            prev = itr
+            itr = itr.next
+            c += 1
+        if index >= c:
+            ll.insertAtEnd(data)
+
+
+
+
+
+    #deleting the data at the start/begining.(queue pop out)
+
+    #deleting data at the end.(stack pop out)
+    #deleting data at the anywhere/index.
+
 
 
     #printing the data.
@@ -39,12 +69,27 @@ class LinkedList:
             print("No data found")
         itr = self.head
         while itr:
-            print(itr.data)
+            print(itr.data,end = ' ')
             itr = itr.next
 
-
     #searching the data.
+    #In linkedlist we have to perform linear search only as
+    #binary search is not possible
+    def search(self, data):
+        itr = self.head
+        index = 1
+        while itr:
+            if itr.data == data: return index
+            itr = itr.next
+            index += 1
+        return 'Number not found'
+
+
+
 ll = LinkedList()
 for _ in range(int(input())):
     ll.insertAtEnd(int(input()))
 ll.print()
+
+print("\n"+ll.search(41))
+
