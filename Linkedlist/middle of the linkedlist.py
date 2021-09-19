5686d54bbeebf1e220231c395000066b9faf722c
Dhananjay <49968050+dhananjaysahu79@users.noreply.github.com>
2021-09-20 01:07:46 +0530

    sc

---
+++ Linkedlist/middle of the linkedlist.py
@@ -0,0 +1,21 @@
+# a naive approach is to find the length of the linkedlist and then find the median + 1
+# Time complexity: O(n + n //2)
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        itr = head
+        c = 0
+        while itr:
+            c += 1
+            itr = itr.next
+        mid = c // 2 + 1
+        itr = head
+        for _ in range(mid-1): itr = itr.next
+        return itr
+# optimal solution is O(n//2) using fast and slow pointers
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow.next if fast.next else slow
